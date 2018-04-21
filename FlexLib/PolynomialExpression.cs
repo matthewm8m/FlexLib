1aca5994bc09cc3d304f96466cd9a249025df159
Matthew Miller <matthewm8m@gmail.com>
2018-04-21 17:11:25 -0600

    Add PolynomialExpression Skeleton

---
+++ FlexLib/PolynomialExpression.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexLib
+{
+    class PolynomialExpression
+    {
+        private PolynomialTerm[] Terms;
+
+        public PolynomialExpression(PolynomialTerm[] terms)
+        {
+            Terms = terms;
+        }
+        public PolynomialExpression(IEnumerable<PolynomialTerm> terms)
+        {
+            Terms = terms.ToArray();
+        }
+
+        public double Evaluate(double indeterminate)
+        {
+            return 0;
+        }
+
+        public PolynomialExpression Derivative()
+        {
+            return null;
+        }
+
+        public PolynomialExpression Antiderivative()
+        {
+            return null;
+        }
+    }
+}
