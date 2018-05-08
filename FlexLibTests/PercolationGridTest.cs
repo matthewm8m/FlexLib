4e3c61c55262eea0093c620ab03b2f5f1a428e0e
Matthew Miller <matthewm8m@gmail.com>
2018-05-07 23:17:50 -0600

    Add PercolationGrid Test Skeleton

---
+++ FlexLibTests/PercolationGridTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FlexLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlexLibTests
+{
+    [TestClass]
+    public class PercolationGridTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDimensions()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestRandomization()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestEvaluation()
+        {
+
+        }
+    }
+}
