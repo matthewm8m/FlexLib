b08562b939ff24b641843b7a39114654785bdbe3
Matthew Miller <matthewm8m@gmail.com>
2019-03-14 21:04:34 -0600

    Implement dummy REPL for FlexLib

---
+++ MathemediaConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathemediaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get user input from user until interrupt.
+            string userInput;
+            while (true)
+            {
+                // Write out input indicator.
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("? ");
+
+                // Get user input until ENTER is pressed.
+                Console.ForegroundColor = ConsoleColor.White;
+                userInput = Console.ReadLine();
+
+                // Exit from program if user inputs 'exit'.
+                if (userInput.ToLower() == "exit")
+                    break;
+
+                // Write out output indicator.
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("! ");
+            }
+            Console.ResetColor();
+        }
+    }
+}
