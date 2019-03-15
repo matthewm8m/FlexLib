64a68648bf20181a0f3d9b81c932d7a42f864dc4
Matthew Miller <matthewm8m@gmail.com>
2019-03-15 10:14:32 -0600

    Add TokenRule class

---
+++ MathemediaConsole/Tokenizer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathemediaConsole
+{
+    public struct TokenRule
+    {
+        public readonly string Pattern;
+        public readonly Func<string, object> Parse;
+
+        public TokenRule(string pattern, Func<string, object> parse)
+        {
+            Pattern = pattern;
+            Parse = parse;
+        }
+    }
+
+    public struct Token
+    {
+
+    }
+
+    public class Tokenizer
+    {
+
+    }
+}
