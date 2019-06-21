6bf713d77943db5065f3f3e6722f98e210a3f24f
Matthew Miller <matthewm8m@gmail.com>
2019-06-21 09:31:40 -0600

    Implement Basic Parser

---
+++ MathemediaConsole/TokenPattern.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathemediaConsole
+{
+    public struct TokenPattern<T>
+    {
+        public readonly string Pattern;
+        public readonly Func<string, T> Transform;
+
+        public TokenPattern(string pattern, Func<string, T> transform)
+        {
+            Pattern = pattern;
+            Transform = transform;
+        }
+    }
+}
