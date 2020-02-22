a9bb05630c21c0b149913786e41914d6ea313d18
Matthew Miller <matthewm8m@gmail.com>
2020-02-21 22:34:15 -0700

    implement field interface

---
+++ FlexLib/Algebra/IField.cs
@@ -0,0 +1,20 @@
+namespace FlexLib.Algebra
+{
+    /// <summary>
+    /// Represents a mathematical field object. This field contains an additive and multiplicative identity called zero and one respectively that can be constructed at any time.
+    /// </summary>
+    /// <typeparam name="T">The type of field element under consideration.</typeparam>
+    public interface IField<T> where T : BaseFieldElement<T>
+    {
+        /// <summary>
+        /// Creates an instance of the additive identity <see cref="BaseFieldElement{T}"/> object of the <see cref="IField{T}"/> object.
+        /// </summary>
+        /// <returns>The additive identity.</returns>
+        T Zero();
+        /// <summary>
+        /// Creates an instance of the multiplicative identity <see cref="BaseFieldElement{T}"/> object of the <see cref="IField{T}"/> object.
+        /// </summary>
+        /// <returns>The multiplicative identity.</returns>
+        T One();
+    }
+}
