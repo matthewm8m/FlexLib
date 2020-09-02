f737ffedbc3e1b6b9431f2652be2e707f74da1c6
Matthew Miller <matthewm8m@gmail.com>
2020-09-02 14:48:57 -0600

    docs: add parser attribute docs

--- FlexLib/Reflection/ParserAttribute.cs
+++ FlexLib/Reflection/ParserAttribute.cs
@@ -2,13 +2,26 @@ using System;
 
 namespace FlexLib.Reflection
 {
+    /// <summary>
+    /// Represents the existance of a common name for a parser than can be referenced outside of the code.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class ParserAttribute : Attribute
     {
+        /// <summary>
+        /// The common name of the parser.
+        /// </summary>
         public readonly string Name;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParserAttribute"/> class with no name.
+        /// </summary>
         public ParserAttribute()
             : this(null) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParserAttribute"/> class with a specified name.
+        /// </summary>
+        /// <param name="name"></param>
         public ParserAttribute(string name)
         {
             Name = name;
