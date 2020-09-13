3941bb42b2d4b8b6da898bec51fb221e7386923f
Matthew Miller <matthewm8m@gmail.com>
2020-09-13 01:40:10 -0600

    feat: implement pattern numbering for token pattern resources

--- FlexLib/Parsing/Tokenizer.cs
+++ FlexLib/Parsing/Tokenizer.cs
@@ -8,8 +8,6 @@ using FlexLib.Reflection;
 
 namespace FlexLib.Parsing
 {
-    public delegate object Parser(string text);
-
     /// <summary>
     /// Uses a specified tokenizer context to provide the rules for taking plain text and converting to tokens.
     /// </summary>
@@ -18,74 +16,31 @@ namespace FlexLib.Parsing
         /// <summary>
         /// Specifies tokenizer rules and settings.
         /// </summary>
-        private readonly TokenizerContext Context;
+        private readonly TokenContext Context;
         /// <summary>
         /// The compiled regular expressions rule constructed from the context.
         /// </summary>
         private readonly Regex Rule;
 
         /// <summary>
-        /// A mapping between textual names and types.
-        /// </summary>
-        private readonly Dictionary<string, Type> TypeMap;
-        /// <summary>
-        /// A mapping between textual names and types and parsers.
-        /// </summary>
-        private readonly Dictionary<Tuple<Type, string>, Parser> ParserMap;
-
-        /// <summary>
         /// Constructs a tokenizer using a specific tokenizer context.
         /// </summary>
         /// <param name="context">The tokenizer context specifying rules and patterns.</param>
-        public Tokenizer(TokenizerContext context)
+        public Tokenizer(TokenContext context)
         {
-            // Set context and compile regular expression rule.
+            // Set context.
             Context = context;
-            Rule = new Regex($@"(?:{
-                    string.Join(@"|", Context.TokenResources.Select(tokenResource => $@"({
-                        string.Join(@"|", tokenResource.TokenPatterns.Select(tokenPattern => $@"(?:{tokenPattern.Pattern})"))
-                        })"))
-                    }|(.))", RegexOptions.Compiled);
 
-            // Initialize the type and parser mappings.
-            InitializeAttributeMaps();
-        }
-
-        /// <summary>
-        /// Initializes mappings between naming attributes and properties.
-        /// </summary>
-        private void InitializeAttributeMaps()
-        {
-            // Search through all types in the current assemblies and compile a list of types and type parsers.
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                foreach (Type type in assembly.GetTypes())
-                {
-                    // Add type mappings.
-                    IEnumerable<TypeAttribute> typeAttrs = type.GetCustomAttributes<TypeAttribute>(false);
-                    foreach (TypeAttribute typeAttr in typeAttrs)
-                        TypeMap.Add(typeAttr.Name, type);
-
-                    // Add parser mappings.
-                    foreach (MethodInfo method in type.GetMethods())
-                    {
-                        // Try to convert method to correct signature.
-                        // If a failure occurs, notify the user that parser is incorrect.
-                        IEnumerable<ParserAttribute> parserAttrs = method.GetCustomAttributes<ParserAttribute>(false);
-                        foreach (ParserAttribute parserAttr in parserAttrs)
-                        {
-                            try
-                            {
-                                ParserMap.Add(new Tuple<Type, string>(type, parserAttr.Name), (Parser)Delegate.CreateDelegate(typeof(Parser), method));
-                            }
-                            catch (Exception)
-                            {
-                                throw new NotSupportedException($"Parser named '{parserAttr.Name}' is not implemented correctly.");
-                            }
-                        }
-                    }
-                }
-            }
+            // Compile the regular expression rule.
+            int RuleIndex = 0;
+            Rule = new Regex(
+                $@"({
+                    string.Join(@"|", Context.Resources.Select(tokenResource => $@"({
+                        string.Join(@"|", tokenResource.Patterns.Select(tokenPattern => $@"(?<{++RuleIndex}>{tokenPattern.RegexPattern})"))
+                    })"))
+                }|(.))",
+                RegexOptions.Multiline | RegexOptions.Compiled | RegexOptions.ExplicitCapture
+            );
         }
 
         public IEnumerable<Token> Tokenize(string input)
@@ -96,26 +51,17 @@ namespace FlexLib.Parsing
             {
                 // For each match, find its corresponding token and emit it.
                 bool tokenized = false;
-                for (int i = 0; i < Context.TokenResources.Count; i++)
+                for (int i = 0; i < Context.Resources.Count; i++)
                 {
                     if (!string.IsNullOrEmpty(match.Groups[i + 1].Value))
                     {
                         // Get the input value and the token resource.
                         string value = match.Groups[i + 1].Value;
-                        TokenResource resource = Context.TokenResources[i];
+                        TokenResource resource = Context.Resources[i];
 
                         // Only emit the token if the ignore flag is not set.
                         if (!resource.Ignore)
                         {
-                            // Get the data type and parsed data if possible.
-                            Type dataType = null;
-                            object dataValue = null;
-                            if (resource.Type != null)
-                            {
-                                if (!TypeMap.TryGetValue(resource.Type, out dataType))
-                                    throw new KeyNotFoundException($"Name error: Could not find type with name '{resource.Type}'.");
-                            }
-
                             yield return new Token(resource.Name, value, resource.Ligature);
                         }
 
