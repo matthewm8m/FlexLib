15141bdc999265d17ae68de29092590aa9213dc0
Matthew Miller <matthewm8m@gmail.com>
2020-12-12 09:48:44 -0700

    tests: refactor constant expression tests to use expression tests helper structure

--- FlexLibTests/ExpressionDom/Expressions/ConstantExpressionTests.cs
+++ FlexLibTests/ExpressionDom/Expressions/ConstantExpressionTests.cs
@@ -10,52 +10,64 @@ namespace FlexLibTests.ExpressionDom.Expressions
     [TestFixture]
     public class ConstantExpressionTests
     {
-        private IList<Tuple<ConstantExpression<int>, int>> TestExpressions;
+        private IList<ExpressionWithExpectation<int>> TestExpressions;
 
         [SetUp]
         protected void SetUp()
         {
-            TestExpressions = new List<Tuple<ConstantExpression<int>, int>>();
+            TestExpressions = new List<ExpressionWithExpectation<int>>();
 
-            TestExpressions.Add(new Tuple<ConstantExpression<int>, int>(1, 1));
-            TestExpressions.Add(new Tuple<ConstantExpression<int>, int>(-2, -2));
-            TestExpressions.Add(new Tuple<ConstantExpression<int>, int>(0, 0));
-            TestExpressions.Add(new Tuple<ConstantExpression<int>, int>(-1548934581, -1548934581));
-            TestExpressions.Add(new Tuple<ConstantExpression<int>, int>(int.MinValue, int.MinValue));
-            TestExpressions.Add(new Tuple<ConstantExpression<int>, int>(int.MaxValue, int.MaxValue));
+            TestExpressions.Add(new ExpressionWithExpectation<int>(
+                new ConstantExpression<int>(1), 1
+            ));
+            TestExpressions.Add(new ExpressionWithExpectation<int>(
+                new ConstantExpression<int>(-2), -2
+            ));
+            TestExpressions.Add(new ExpressionWithExpectation<int>(
+                new ConstantExpression<int>(0), 0
+            ));
+            TestExpressions.Add(new ExpressionWithExpectation<int>(
+                new ConstantExpression<int>(-1548934581), -1548934581
+            ));
+            TestExpressions.Add(new ExpressionWithExpectation<int>(
+                new ConstantExpression<int>(int.MinValue), int.MinValue
+            ));
+            TestExpressions.Add(new ExpressionWithExpectation<int>(
+                new ConstantExpression<int>(int.MaxValue), int.MaxValue
+            ));
         }
 
         [Test]
         public void TestIsEvaluable()
         {
-            foreach (Tuple<ConstantExpression<int>, int> pair in TestExpressions)
+            foreach (ExpressionWithExpectation<int> exprExpect in TestExpressions)
             {
-                IExpression<int> expr = pair.Item1;
-                int value = pair.Item2;
+                IExpression<int> expression = exprExpect.Expression;
+                int expected = exprExpect.ExpectedValue;
 
-                Assert.IsTrue(expr.IsEvaluable());
+                Assert.IsTrue(expression.IsEvaluable());
             }
         }
         [Test]
         public void TestEvaluate()
         {
-            foreach (Tuple<ConstantExpression<int>, int> pair in TestExpressions)
+            foreach (ExpressionWithExpectation<int> exprExpect in TestExpressions)
             {
-                IExpression<int> expr = pair.Item1;
-                int value = pair.Item2;
+                IExpression<int> expression = exprExpect.Expression;
+                int expected = exprExpect.ExpectedValue;
 
-                Assert.AreEqual(expr.Evaluate(), value);
+                Assert.AreEqual(expected, expression.Evaluate());
             }
         }
         [Test]
         public void TestValue()
         {
-            foreach (Tuple<ConstantExpression<int>, int> pair in TestExpressions)
+            foreach (ExpressionWithExpectation<int> exprExpect in TestExpressions)
             {
-                ConstantExpression<int> expr = pair.Item1;
-                int value = pair.Item2;
+                ConstantExpression<int> expression = exprExpect.Expression as ConstantExpression<int>;
+                int expected = exprExpect.ExpectedValue;
 
-                Assert.AreEqual(expr.Value, value);
+                Assert.AreEqual(expected, expression.Value);
             }
         }
     }
