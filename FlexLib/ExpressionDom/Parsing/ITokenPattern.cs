fe28251f46109b968c44acf63363d0e6b5e5f558
Matthew Miller <matthewm8m@gmail.com>
2020-12-19 09:26:56 -0700

    refactor: change itokenpattern classes to return the number of tokens to consume instead of whether a match was found

--- FlexLib/ExpressionDom/Parsing/ITokenPattern.cs
+++ FlexLib/ExpressionDom/Parsing/ITokenPattern.cs
@@ -11,8 +11,7 @@ namespace FlexLib.ExpressionDom.Parsing
         /// Determines whether the <see cref="ITokenPattern"/> object matches a specified collection of tokens.
         /// </summary>
         /// <param name="tokens">The collections of tokens to check for a match. Matches are checked strictly from the beginning of the collection.</param>
-        /// <param name="count">The number of tokens that match the pattern from the start of the enumerable.</param>
-        /// <returns><c>true</c> if the pattern matches the tokens; otherwise, <c>false</c>.</returns>
-        bool IsMatch(IEnumerable<Token> tokens, out int count);
+        /// <returns>The number of tokens that match the pattern from the start of the enumerable. This is set to <c>0</c> if there is no match.</returns>
+        int FindMatch(IEnumerable<Token> tokens);
     }
 }
--- FlexLib/ExpressionDom/Parsing/TokenRulePattern.cs
+++ FlexLib/ExpressionDom/Parsing/TokenRulePattern.cs
@@ -17,9 +17,8 @@ namespace FlexLib.ExpressionDom.Parsing
         /// Determines whether the <see cref="TokenRulePattern"/> object matches a specified collection of tokens.
         /// </summary>
         /// <param name="tokens">The collections of tokens to check for a match. Matches are checked strictly from the beginning of the collection.</param>
-        /// <param name="count">The number of tokens that match the pattern from the start of the enumerable. Set to <c>1</c> if a match was successful; otherwise, set to <c>0</c>.</param>
-        /// <returns><c>true</c> if the <see cref="Rule"/> matches the first token's <see cref="Token.Definition"/>; otherwise, <c>false</c>.</returns>
-        public virtual bool IsMatch(IEnumerable<Token> tokens, out int count)
+        /// <returns>The number of tokens that match the pattern from the start of the enumerable. Set to <c>1</c> if a match was successful; otherwise, set to <c>0</c>.</returns>
+        public virtual int FindMatch(IEnumerable<Token> tokens)
         {
             // In order to match the given enumerable, there must be at least 1 token and it must match the specified rule.
             if (tokens != null && tokens.Any())
@@ -27,14 +26,12 @@ namespace FlexLib.ExpressionDom.Parsing
                 if (Rule == null || Rule == tokens.First().Definition)
                 {
                     // We eat the first token if it matches.
-                    count = 1;
-                    return true;
+                    return 1;
                 }
             }
 
             // We eat no tokens if there is no match.
-            count = 0;
-            return false;
+            return 0;
         }
     }
 
@@ -53,52 +50,46 @@ namespace FlexLib.ExpressionDom.Parsing
         /// Determines whether the <see cref="TokenRulePattern{T}"/> object matches a specified collection of tokens.
         /// </summary>
         /// <param name="tokens">The collections of tokens to check for a match. Matches are checked strictly from the beginning of the collection.</param>
-        /// <param name="count">The number of tokens that match the pattern from the start of the enumerable. Set to <c>1</c> if a match was successful; otherwise, set to <c>0</c>.</param>
-        /// <returns><c>true</c> if the <see cref="TokenRulePattern.Rule"/> matches the first token's <see cref="Token.Definition"/> and the first token's <see cref="Token.Value"/> has type <c>T</c>; otherwise, <c>false</c>.</returns>
-        public override bool IsMatch(IEnumerable<Token> tokens, out int count)
+        /// <returns>The number of tokens that match the pattern from the start of the enumerable. Set to <c>1</c> if a match was successful; otherwise, set to <c>0</c>.</returns>
+        public override int FindMatch(IEnumerable<Token> tokens)
         {
             // We check if the base match conditions are satisfied first. If they are, we additionally require the token value type to match. 
-            bool match = base.IsMatch(tokens, out _);
+            bool match = base.FindMatch(tokens) > 0;
             if (match)
             {
                 if (tokens.First().Value is T)
                 {
                     // We eat the first token if it matches.
-                    count = 1;
-                    return true;
+                    return 1;
                 }
             }
 
             // We eat no tokens if there is no match.
-            count = 0;
-            return false;
+            return 0;
         }
         /// <summary>
         /// Determines whether the <see cref="TokenRulePattern{T}"/> object matches a specified collection of tokens.
         /// </summary>
         /// <param name="tokens">The collections of tokens to check for a match. Matches are checked strictly from the beginning of the collection.</param>
-        /// <param name="count">The number of tokens that match the pattern from the start of the enumerable. Set to <c>1</c> if a match was successful; otherwise, set to <c>0</c>.</param>
         /// <param name="value">The value of the token if it matched.</param>
-        /// <returns><c>true</c> if the <see cref="TokenRulePattern.Rule"/> matches the first token's <see cref="Token.Definition"/> and the first token's <see cref="Token.Value"/> has type <c>T</c>; otherwise, <c>false</c>.</returns>
-        public bool IsMatch(IEnumerable<Token> tokens, out int count, out T value)
+        /// <returns>The number of tokens that match the pattern from the start of the enumerable. Set to <c>1</c> if a match was successful; otherwise, set to <c>0</c>.</returns>
+        public int FindMatch(IEnumerable<Token> tokens, out T value)
         {
             // We check if the base match conditions are satisfied first. If they are, we additionally require the token value type to match. 
-            bool match = base.IsMatch(tokens, out _);
+            bool match = base.FindMatch(tokens) > 0;
             if (match)
             {
                 if (tokens.First().Value is T tokenValue)
                 {
                     // We eat the first token if it matches.
-                    count = 1;
                     value = tokenValue;
-                    return true;
+                    return 1;
                 }
             }
 
             // We eat no tokens if there is no match.
-            count = 0;
             value = default(T);
-            return false;
+            return 0;
         }
     }
 }
