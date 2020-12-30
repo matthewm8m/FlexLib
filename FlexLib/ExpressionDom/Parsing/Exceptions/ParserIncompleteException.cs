edcfe8cf48712762c4d90667c5622a7bbc75a6eb
Matthew Miller <matthewm8m@gmail.com>
2020-12-30 09:42:29 -0700

    feat: add parser incomplete exception skeleton

---
+++ FlexLib/ExpressionDom/Parsing/Exceptions/ParserIncompleteException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FlexLib.ExpressionDom.Parsing
+{
+    /// <summary>
+    /// Represents an exception that prevented the parser from completing its operation.
+    /// </summary>
+    public class ParserIncompleteException : Exception
+    {
+
+    }
+}
