1c59e121b7455fd803f62be74aadc8ecf1571d5b
Matthew Miller <matthewm8m@gmail.com>
2021-01-01 18:24:15 -0700

    feat: implement parser multiple parse method

--- FlexLib/ExpressionDom/Parsing/Parser.cs
+++ FlexLib/ExpressionDom/Parsing/Parser.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FlexLib.ExpressionDom.Parsing
 {
@@ -15,7 +16,7 @@ namespace FlexLib.ExpressionDom.Parsing
         /// <param name="levels">The ordered parser levels including all parser rules.</param>
         public Parser(IEnumerable<ParserLevel> levels)
         {
-
+            Levels = new List<ParserLevel>(levels);
         }
 
         /// <summary>
@@ -25,7 +26,119 @@ namespace FlexLib.ExpressionDom.Parsing
         /// <returns>The output reduced token stream.</returns>
         public IEnumerable<Token> Parse(IEnumerable<Token> tokens)
         {
-            yield break;
+            // We use this function to step forward from a particular node in the token stream.
+            IEnumerable<Token> EnumerateFromNode(LinkedListNode<Token> node)
+            {
+                while (node != null)
+                {
+                    yield return node.Value;
+                    node = node.Next;
+                }
+            }
+
+            // We use a linked list because it has efficient insertion and deletion.
+            LinkedList<Token> tokenStream = new LinkedList<Token>(tokens);
+
+            // Perform as much reduction as possible before returning.
+            bool reducible = true;
+            while (reducible)
+            {
+                // We assume that the expression is not reducible every interation until a reduction takes place.
+                // This allows us to continue reducing only if a parser rule was triggered in the previous iteration.
+                reducible = false;
+                foreach (ParserLevel level in Levels)
+                {
+                    // We determine the starting location of the token stream depending on the level associativity.
+                    LinkedListNode<Token> tokenNode = null;
+                    switch (level.Associativity)
+                    {
+                        case ParserAssociativity.LeftToRight:
+                            tokenNode = tokenStream.First;
+                            break;
+                        case ParserAssociativity.RightToLeft:
+                            tokenNode = tokenStream.Last;
+                            break;
+                    }
+
+                    // We make a full pass across the token stream exactly once per parser level.
+                    while (tokenNode != null)
+                    {
+                        // We apply each parser rule in the level per location in the token stream.
+                        foreach (ParserRule rule in level.Rules)
+                        {
+                            // We determine how many tokens are matched by the current rule starting from our head node.
+                            IEnumerable<Token> streamHead = EnumerateFromNode(tokenNode);
+                            int matchedTokens = rule.Pattern.FindMatch(streamHead);
+
+                            /*
+                                If there are matched tokens, we need to replace the matched tokens with the new tokens
+                                provided by the rule. We make sure to set the flag to indicate a reduction has taken
+                                place. We also need consistent behavior for where the new head node is located. When
+                                there are new tokens, we move to their beginning (based on associativity). Otherwise, we
+                                move further in the token stream (based on associativity).
+                            */
+                            if (matchedTokens > 0)
+                            {
+                                // Insert new tokens, if any.
+                                LinkedListNode<Token> tokenNodeReplacement = null;
+                                foreach (Token token in rule.Tokenize(streamHead.Take(matchedTokens)))
+                                {
+                                    LinkedListNode<Token> tokenNodeNew = tokenStream.AddBefore(tokenNode, token);
+                                    switch (level.Associativity)
+                                    {
+                                        case ParserAssociativity.LeftToRight:
+                                            tokenNodeReplacement = tokenNodeReplacement ?? tokenNodeNew;
+                                            break;
+                                        case ParserAssociativity.RightToLeft:
+                                            tokenNodeReplacement = tokenNodeNew;
+                                            break;
+                                    }
+                                }
+
+                                // Delete old tokens and move token head.
+                                for (int k = 0; k < matchedTokens - 1; k++)
+                                    tokenStream.Remove(tokenNode.Next);
+                                if (tokenNodeReplacement == null)
+                                {
+                                    switch (level.Associativity)
+                                    {
+                                        case ParserAssociativity.LeftToRight:
+                                            tokenNodeReplacement = tokenNode.Next;
+                                            break;
+                                        case ParserAssociativity.RightToLeft:
+                                            tokenNodeReplacement = tokenNode.Previous;
+                                            break;
+                                    }
+                                }
+                                tokenStream.Remove(tokenNode);
+                                tokenNode = tokenNodeReplacement;
+
+                                // Set reduction flag.
+                                reducible = true;
+                                continue;
+                            }
+                        }
+
+                        // We need to move our token stream head in the correct direction based on the level associativity.
+                        switch (level.Associativity)
+                        {
+                            case ParserAssociativity.LeftToRight:
+                                tokenNode = tokenNode.Next;
+                                break;
+                            case ParserAssociativity.RightToLeft:
+                                tokenNode = tokenNode.Previous;
+                                break;
+                        }
+                    }
+
+                    // If a reduction happened in this level, we don't want to move forward to levels with lower
+                    // precedence until we are certain that we are done with higher precedence levels.
+                    if (reducible)
+                        break;
+                }
+            }
+
+            return tokenStream;
         }
         /// <summary>
         /// Performs parsing to reduce a token stream into a single token. If the parsing cannot reduce the token stream to a single token, a <see cref="ParserIncompleteException"/> is thrown.
