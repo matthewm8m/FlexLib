8c003704dc06de6f8625d096019f3dfc94a9ff87
Matthew Miller <matthewm8m@gmail.com>
2021-01-02 17:05:51 -0700

    feat: update mathemedia console with new parsing

--- MathemediaConsole/Program.cs
+++ MathemediaConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
+
+using FlexLib.Algebra;
+using FlexLib.ExpressionDom.Expressions;
+using FlexLib.ExpressionDom.Parsing;
 
 namespace MathemediaConsole
 {
@@ -8,91 +10,43 @@ namespace MathemediaConsole
     {
         static void Main(string[] args)
         {
+            // Set the indicators for prompts.
             string inputIndicator = "? ";
             string outputIndicator = "! ";
-            
-            TokenRule tokenRuleInt = new TokenRule(@"\d+", s => int.Parse(s));
-            TokenRule tokenRuleOperator = new TokenRule(@"[=\+\-\*\\]", s => s);
-            TokenRule tokenRuleUnknown = new TokenRule(@".", s => null);
-            Tokenizer tokenizer = new Tokenizer(new List<TokenRule> { tokenRuleInt, tokenRuleOperator, tokenRuleUnknown });
+            ConsoleColor indicatorColor = ConsoleColor.Cyan;
 
-            // Get user input from user until interrupt.
-            StringBuilder userInput = new StringBuilder();
-            int userIndex = 0;
+            /* REPL */
+            // Loop
             while (true)
             {
-                // Write out input indicator.
-                Console.ForegroundColor = ConsoleColor.Gray;
+                // Read
+                Console.ForegroundColor = indicatorColor;
                 Console.Write(inputIndicator);
+                Console.ResetColor();
+                string input = Console.ReadLine();
 
-                // Get user input until ENTER is pressed.
-                do
+                // Evaluate
+                string output;
+                try
                 {
-                    // Get user input character by character.
-                    ConsoleKeyInfo userKey = Console.ReadKey();
+                    Token result = Pipeline.DefaultPipeline.ParseSingular(input);
+                    output = ((IExpression<RealFieldElement>)result.Value).Evaluate().ToString();
+                }
+                catch (LexerSyntaxException lexEx)
+                {
+                    output = $"{lexEx.Message}: {lexEx.TokenSource.Snippet}";
+                }
+                catch (ParserIncompleteException parEx)
+                {
+                    output = $"{parEx.Message}: {parEx.TokenSource.Snippet}";
+                }
 
-                    // Break input loop when ENTER is pressed.
-                    if (userKey.Key == ConsoleKey.Enter)
-                        break;
-
-                    // Check key for special key codes.
-                    switch (userKey.Key)
-                    {
-                        case ConsoleKey.LeftArrow:
-                            userIndex = (userIndex == 0) ? userIndex : userIndex - 1;
-                            break;
-                        case ConsoleKey.RightArrow:
-                            userIndex = (userIndex == userInput.Length) ? userIndex : userIndex + 1;
-                            break;
-                        case ConsoleKey.Backspace:
-                            if (userIndex != 0)
-                            {
-                                userIndex = userIndex - 1;
-                                userInput.Remove(userIndex, 1);
-                            }
-                            break;
-                        case ConsoleKey.Delete:
-                            if (userIndex != userInput.Length)
-                            {
-                                userInput.Remove(userIndex, 1);
-                            }
-                            break;
-                        default:
-                            userInput.Insert(userIndex, userKey.KeyChar);
-                            userIndex++;
-                            break;
-                    }
-
-                    // Get tokens and write them with color.
-                    Console.CursorLeft = inputIndicator.Length;
-                    for (int i = 0; i < userInput.Length + 1; i++)
-                    {
-                        Console.Write(" ");
-                    }
-                    Console.CursorLeft = inputIndicator.Length;
-                    foreach (Token token in tokenizer.Tokenize(userInput.ToString()))
-                    {
-                        if (token.Value is int)
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                        else if (token.Value is string)
-                            Console.ForegroundColor = ConsoleColor.Green;
-                        else if (token.Value is null)
-                            Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.Write(token.Text);
-                    }
-                    Console.CursorLeft = inputIndicator.Length + userIndex;
-                } while (true);
-
-                // Write out output indicator.
-                userInput.Clear();
-                userIndex = 0;
-                Console.CursorLeft = 0;
-                Console.CursorTop = Console.CursorTop + 1;
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.WriteLine(outputIndicator);
+                // Print
+                Console.ForegroundColor = indicatorColor;
+                Console.Write(outputIndicator);
+                Console.ResetColor();
+                Console.WriteLine(output);
             }
-            Console.ResetColor();
         }
     }
 }
